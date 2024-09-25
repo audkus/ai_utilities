d59dd99058878c4dcc1920816aa9036f1925736e
rkw745 <steffen.rasmussen@adm.ku.dk>
2024-09-25 15:34:09 +0200

    renamed module folder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ai_utilities',
+    name='ai_utilities_audkus',
     version='0.2.0',
     packages=find_packages(),
     install_requires=[
