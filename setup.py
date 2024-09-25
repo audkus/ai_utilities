cc8ffc82e233eb3a482e856c73d1551f12f153ab
rkw745 <steffen.rasmussen@adm.ku.dk>
2024-09-25 15:42:54 +0200

    Changed name of app

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ai_utilities_audkus',
+    name='ai_utilities',
     version='0.2.0',
     packages=find_packages(),
     install_requires=[
